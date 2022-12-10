1aea1e64b02899d556a19df89700c6a0dbc23195
Lukman <luqmannur33@gmail.com>
2022-12-10 23:08:57 +0700

    update login form

--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -1,31 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import {
-  Container,
-  Alert,
-  Image,
-  Row,
-  Card,
-  Col,
-  Form,
-  Button,
-} from 'react-bootstrap';
+import { Container, Alert, Image, Row, Card, Col, Form, Button, Modal } from 'react-bootstrap';
 import { LoginUser, reset } from '../../features/authSlice';
 import './LoginPage.css';
 import { LogoLogin } from '../../materials';
 
 const LoginForm = () => {
+  const [showDemo, setShowDemo] = useState(false);
+  const handleClose = () => setShowDemo(false);
+  const handleShow = () => setShowDemo(true);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [show, setShow] = useState(true);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const {
-    user, isError, isSuccess, isLoading, message
-  } = useSelector(
-    (state) => state.auth
-  );
+  const { user, isError, isSuccess, isLoading, message } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (user || isSuccess) {
@@ -48,63 +39,65 @@ const LoginForm = () => {
       <Card className="py-4 my-4 shadow">
         <Row>
           <Col md={6} className="d-none d-md-block">
-            <Image
-              src={LogoLogin}
-              alt="login form"
-              className="rounded-start fluid"
-            />
+            <Image src={LogoLogin} alt="login form" className="rounded-start fluid" />
           </Col>
           <Col md={6} className="my-auto">
             <Card.Body className="d-flex flex-column">
               <div className="d-flex justify-content-center flex-row mt-2">
                 <span className="h1 fw-bold mb-0 title">Selamat datang</span>
               </div>
-              <h5
-                className="fw-normal text-center my-4 pb-3"
-                style={{ letterSpacing: '1px' }}
-              >
+              <h5 className="fw-normal text-center my-4 pb-3" style={{ letterSpacing: '1px' }}>
                 Masuk untuk mengakses ruang Anda
               </h5>
               <Form onSubmit={Auth}>
                 {isError && (
-                  <Alert
-                    show={show}
-                    variant="danger"
-                    className="text-center"
-                    onClose={() => setShow(false)}
-                    dismissible
-                  >
+                  <Alert show={show} variant="danger" className="text-center" onClose={() => setShow(false)} dismissible>
                     <Alert.Heading>{message}</Alert.Heading>
                   </Alert>
                 )}
                 <Form.Group className="mb-3">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Masukan Email"
-                  />
+                  <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Masukan Email" />
                 </Form.Group>
                 <Form.Group className="mb-3">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Masukan Password"
-                  />
+                  <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Masukan Password" />
                 </Form.Group>
-                <Button
-                  className="mb-4 px-5"
-                  type="submit"
-                  variant="success"
-                  size="md"
-                  onClick={() => setShow(true)}
-                >
+                <Button className="mb-4 px-5 w-100" type="submit" variant="success" onClick={() => setShow(true)}>
                   {isLoading ? 'Loading...' : 'Masuk'}
                 </Button>
               </Form>
+              <Button variant="outline-success" onClick={handleShow}>
+                Akun Demo
+              </Button>
+
+              <Modal show={showDemo} size="lg" centered onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Daftar Akun Demo</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Row>
+                    <Col md={4}>
+                      <p className="fw-semibold">Admin</p>
+                      <p>
+                        Email: admin@gmail.com <br /> Password: 123456
+                      </p>
+                    </Col>
+                    <Col md={4}>
+                      <p className="fw-semibold">Staf</p>
+                      <p>
+                        Email: staf@gmail.com <br /> Password: 123456
+                      </p>
+                    </Col>
+                    <Col md={4}>
+                      <p className="fw-semibold">User</p>
+                      <p>
+                        Email: user@gmail.com <br /> Password: 123456
+                      </p>
+                    </Col>
+                  </Row>
+                </Modal.Body>
+              </Modal>
             </Card.Body>
           </Col>
         </Row>
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Alert, Container } from 'react-bootstrap';
 import { LoginHeader, LoginForm, LoginFooter } from '../../components';
 
 const LoginPage = () => {
   return (
     <>
-    <LoginHeader />
-    <LoginForm />
-    <LoginFooter />
+      <LoginHeader />
+      <Container>
+        <Alert key="warning" variant="warning" className="text-center mt-3">
+          Jika tidak bisa login, mohon coba kembali menggunakan browser <strong>Mozilla Firefox</strong>
+        </Alert>
+      </Container>
+      <LoginForm />
+      <LoginFooter />
     </>
   );
 };
